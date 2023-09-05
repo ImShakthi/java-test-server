aaf183fc522897ee633ad57f371244f28df2458b
Sakthivel Balasubramaniam <ideamatex@gmail.com>
2023-09-05 02:25:55 +0200

    add simple log

--- src/main/java/com/imshakthi/javatestserver/kafka/producer/SampleProducer.java
+++ src/main/java/com/imshakthi/javatestserver/kafka/producer/SampleProducer.java
@@ -2,12 +2,17 @@ package com.imshakthi.javatestserver.kafka.producer;
 
 
 import com.imshakthi.javatestserver.kafka.config.KafkaConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class SampleProducer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(SampleProducer.class);
 
     private final KafkaTemplate<String, String> kafkaSampleTemplate;
 
@@ -20,6 +25,13 @@ public class SampleProducer {
     }
 
     public void sendMessage(String msg) {
-        kafkaSampleTemplate.send(kafkaConfig.getMessageWriterTopicName(), msg);
+        kafkaSampleTemplate.send(kafkaConfig.getMessageWriterTopicName(), msg)
+            .whenComplete((result, err) -> {
+                if (err == null) {
+                    LOGGER.info("MESSAGE:: '{}' sent successfully.", msg);
+                } else {
+                    LOGGER.error("ERROR in getting message '{}' due to {}", msg, err.getMessage());
+                }
+            });
     }
 }

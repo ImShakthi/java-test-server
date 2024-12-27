8e59f15eff7d5f41781ca1fb9bc4af34bff2fb9d
Sakthivel Balasubramaniam <ideamatex@gmail.com>
2024-12-27 11:44:10 +0100

    fix runtime error

--- src/main/java/com/imshakthi/javatestserver/JavaTestServerApplication.java
+++ src/main/java/com/imshakthi/javatestserver/JavaTestServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTestServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(JavaTestServerApplication.class, args);
     }
 
